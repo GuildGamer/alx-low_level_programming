09fc27438f9c0c3afd11a02bf70709a33194c251
Steven Umar <tobi4steve@gmail.com>
2022-09-09 00:11:33 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: 1
+ *main - print a string to standard error
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
  */
-
 int main(void)
 {
-char s[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-fprintf (stderr, "%s", s);
+char m[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", m);
 return (1);
-}
