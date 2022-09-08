189a8543b00a4ab3da0651d2e17ad984d79d5546
Steven Umar <tobi4steve@gmail.com>
2022-09-09 00:10:09 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,8 +7,8 @@
 
 int main(void)
 {
-	char str1[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char s[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf (stderr, "%s", str1);
-	return (1);
+fprintf (stderr, "%s", s);
+return (1);
 }
