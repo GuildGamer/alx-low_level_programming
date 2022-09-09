6ec01feb4974d7e9e508b43024c4561d3f12f1e2
Steven Umar <tobi4steve@gmail.com>
2022-09-10 00:56:35 +0100

    commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
+ */
+int main(void)
+{
+	char i;
+	char comma = ',';
+	char space = ' ';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i < '9')
+			putchar(comma);
+			putchar(space);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
