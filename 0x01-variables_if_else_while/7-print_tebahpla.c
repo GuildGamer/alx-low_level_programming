1ce9db43c569f0b784fae97fc9cbd4bb98f4632c
Steven Umar <tobi4steve@gmail.com>
2022-09-10 00:31:54 +0100

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
