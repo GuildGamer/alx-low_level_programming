98678b270b9fbdb6226018a835d111d02ca72ffb
Steven Umar <tobi4steve@gmail.com>
2022-09-10 02:47:48 +0100

    new

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int comma = ',';
+	int space = ' ';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '1'; l <= '9'; l++)
+				{
+					if ((i + j) < (k + l))
+					{
+					putchar(i);
+					putchar(j);
+
+					putchar(space);
+
+					putchar(k);
+					putchar(l);
+
+					if ((i < '9' || j < '8') && (k < '9' || l < '9'))
+					{
+						putchar(comma);
+						putchar(space);
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
