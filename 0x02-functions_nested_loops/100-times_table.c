1b0d894d002ce3442264c758332a3c0ddf109842
Steven Umar <tobi4steve@gmail.com>
2022-09-14 01:25:32 +0100

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  * in order separated by a comma followed by a space.
  * @n: The number to begin counting at.
  */
-void print_to_times_table(int n)
+void print_times_table(int n)
 {
 	if (n > 0 && n < 15)
 	{
