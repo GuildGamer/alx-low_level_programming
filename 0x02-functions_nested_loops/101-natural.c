8d2ede2349d43d03cc0114ff97b4305cda87a254
Steven Umar <tobi4steve@gmail.com>
2022-09-14 23:05:46 +0100

    new

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,13 +10,16 @@
 int main(void)
 {
 	int i = 0;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("%d\n", i);
+			sum += i;
 		}
 	}
+	printf("%d\n", i);
+
 	return (0);
 }
