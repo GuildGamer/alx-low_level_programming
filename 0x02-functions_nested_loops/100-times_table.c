cd2cc30784e6913a5703777b225b9e994bb57b9c
Steven Umar <tobi4steve@gmail.com>
2022-09-14 01:29:37 +0100

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_to_times_table - Prints all natural numbers from input to 98,
+ * print_times_table - Prints all natural numbers from input to 98,
  * in order separated by a comma followed by a space.
  * @n: The number to begin counting at.
  */
 void print_times_table(int n)
 {
-	if (n > 0 && n < 15)
+	if (n > 0 && n <= 15)
 	{
 		int i = 0;
 
